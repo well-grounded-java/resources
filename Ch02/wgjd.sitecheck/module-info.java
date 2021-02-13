3a8ce8be21af83c7383c7eded2c4fd30bf18d3ee
Ben Evans <ben@kathik.co.uk>
2021-02-13 14:20:17 +0100

    Sync

--- Ch02/wgjd.sitecheck/module-info.java
+++ Ch02/wgjd.sitecheck/module-info.java
@@ -1,5 +1,6 @@
 module wgjd.sitecheck {
-  // requires java.net.http;
+  requires java.net.http;
+
   exports wgjd.sitecheck;
   exports wgjd.sitecheck.concurrent;
 }
---
+++ Ch05/ch05/COWExamples.java
@@ -0,0 +1,10 @@
+package ch05;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class COWExamples {
+}
