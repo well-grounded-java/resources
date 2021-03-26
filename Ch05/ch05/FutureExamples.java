05049722f6d25301b78e3a22dc6c4d8d77fb1b14
Ben Evans <benjamin.john.evans@gmail.com>
2021-03-26 10:27:00 +0100

    Add ch5 code (#5)
    * Dictionary
    
    * For class
    
    * New dictionary impls
    
    * Sync
    
    * Waypoint
    
    * Examples
    
    Co-authored-by: Ben Evans <ben@kathik.co.uk>

---
+++ Ch05/ch05/FutureExamples.java
@@ -0,0 +1,36 @@
+package ch05;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class FutureExamples {
+
+    public static void main(String[] args) {
+        Future<Long> fut = getNthPrime(1_000_000_000);
+        Long result = null;
+
+        while (result == null) {
+            try {
+                result = fut.get(60, TimeUnit.SECONDS);
+            } catch (TimeoutException tox) {
+              // Timed out - nothing to do
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+                break;
+            }
+
+            System.out.println("Still not found the billionth prime!");
+        }
+
+        System.out.println("Found it: "+ result.longValue());
+    }
+
+    private static Future<Long> getNthPrime(int i) {
+        return null;
+    }
+}
