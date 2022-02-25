04ba5fdaf91cf9688ed3bb43f59089d161b9b92e
Ben Evans <benjamin.john.evans@gmail.com>
2022-02-25 17:42:13 +0100

    Tweaks

--- Ch16/src/ch16/clj/PersistentExamples.java
+++ Ch16/src/ch16/clj/PersistentExamples.java
@@ -24,30 +24,25 @@ public class PersistentExamples {
 
     private void run3() {
         var vector = PersistentVector.EMPTY;
-        for (int i = 0; i < 1024; i = i + 1) {
-            vector = vector.cons(i);
-        }
-//        System.out.println(Arrays.toString(vector.tail));
-//        System.out.println(Arrays.toString(vector.root.array));
-//        System.out.println("vector.root.array[0] : " + Arrays.toString(((PersistentVector.Node) (vector.root.array[0])).array));
-//        System.out.println("vector.root.array[30]: " + Arrays.toString(((PersistentVector.Node) (vector.root.array[30])).array));
-//        System.out.println("----------------");
-        for (int i = 1024; i < 1088; i = i + 1) {
+        for (int i = 0; i < 1088; i = i + 1) {
             vector = vector.cons(i);
         }
         System.out.println(Arrays.toString(vector.tail));
         System.out.println(Arrays.toString(vector.root.array));
+        System.out.println();
+
         var vRA0 = (PersistentVector.Node) (vector.root.array[0]);
         var vRA1 = (PersistentVector.Node) (vector.root.array[1]);
-        System.out.println("vector.root.array[0] : " + Arrays.toString(vRA0.array));
-        System.out.println("vector.root.array[1] : " + Arrays.toString(vRA1.array));
+        System.out.println("r.a[0] : " + Arrays.toString(vRA0.array));
+        System.out.println("r.a[1] : " + Arrays.toString(vRA1.array));
+        System.out.println();
 
         var vRA0A0 = (PersistentVector.Node)(((PersistentVector.Node)vRA0).array[0]);
         var vRA0A31 = (PersistentVector.Node)(((PersistentVector.Node)vRA0).array[31]);
         var vRA1A0 = (PersistentVector.Node)(((PersistentVector.Node)vRA1).array[0]);
-        System.out.println("vector.root.array[0].array[0] : " + Arrays.toString(vRA0A0.array));
-        System.out.println("vector.root.array[0].array[31] : " + Arrays.toString(vRA0A31.array));
-        System.out.println("vector.root.array[1].array[0] : " + Arrays.toString(vRA1A0.array));
+        System.out.println("r.a[0].a[0] : " + Arrays.toString(vRA0A0.array));
+        System.out.println("r.a[0].a[31] : " + Arrays.toString(vRA0A31.array));
+        System.out.println("r.a[1].a[0] : " + Arrays.toString(vRA1A0.array));
     }
 
     private void run2() {
