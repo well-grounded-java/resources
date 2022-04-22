89e66170d13e39e0a112400ba56b868898624a16
Jason R. Clark <jason@jasonrclark.net>
2022-04-22 09:56:23 +0200

    Data driven examples for Spek

--- Ch14/spek-maven/src/test/kotlin/com/wellgrounded/InMemoryCachedPriceSpek.kt
+++ Ch14/spek-maven/src/test/kotlin/com/wellgrounded/InMemoryCachedPriceSpek.kt
@@ -3,6 +3,7 @@ package com.wellgrounded
 import org.spekframework.spek2.Spek
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import java.math.BigDecimal
 
@@ -20,6 +21,12 @@ object InMemoryCachedPriceSpek : Spek({
             val first = cachedPrice.initialPrice
             val second = cachedPrice.initialPrice
             assertTrue(first === second)
+        }
+
+        listOf(1, 2, 3).forEach {
+            test("testing $it") {
+                assertNotEquals(BigDecimal(it), cachedPrice.initialPrice)
+            }
         }
     }
 
