68c9b79bb97cee09aa3e860b090ec923490b24ae
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2022-05-19 11:12:53 +0300

    The previous loop did not make any sense. Once we time out, we want to exit and cancel whatever is currently running (which means that the getNthPrime() must be aware of interruptions too).

--- Ch06/ch06/FutureExamples.java
+++ Ch06/ch06/FutureExamples.java
@@ -1,38 +1,38 @@
 package ch06;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 
 public class FutureExamples {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Future<Long> fut = getNthPrime(1_000_000_000);
-        Long result = null;
-
-        while (result == null) {
-            try {
-                result = fut.get(60, TimeUnit.SECONDS);
-            } catch (TimeoutException tox) {
-                // Timed out - better cancel the task
-                System.err.println("Task timed out, cancelling");
-                fut.cancel(true);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                break;
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-                break;
-            }
-
-            System.out.println("Still not found the billionth prime!");
+        try {
+            long result = fut.get(1, TimeUnit.MINUTES);
+            System.out.println("Found it: " + result);
+        } catch (TimeoutException tox) {
+            // Timed out - better cancel the task
+            System.err.println("Task timed out, cancelling");
+            fut.cancel(true);
+        } catch (InterruptedException e) {
+            fut.cancel(true);
+            throw e;
+        } catch (ExecutionException e) {
+            fut.cancel(true);
+            e.getCause().printStackTrace();
         }
-
-        System.out.println("Found it: "+ result.longValue());
     }
 
     private static Future<Long> getNthPrime(int i) {
-        return null;
+        var task = new FutureTask<>(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(100);
+                return (long)Integer.MAX_VALUE;
+            } catch (InterruptedException e) {
+                System.err.println("Cancelling search");
+                throw new CancellationException("interrupted");
+            }
+        });
+        new Thread(task).start();
+        return task;
     }
 }
